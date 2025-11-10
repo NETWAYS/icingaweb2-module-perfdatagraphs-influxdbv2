d51f6d070ac7401974dfda3744f15deaa75a7a1e
Markus Opolka <markus.opolka@netways.de>
2025-11-10 16:59:49 +0100

    PoC Calculate aggregation window

--- library/Perfdatagraphsinfluxdbv2/Client/Influx.php
+++ library/Perfdatagraphsinfluxdbv2/Client/Influx.php
@@ -2,8 +2,13 @@
 
 namespace Icinga\Module\Perfdatagraphsinfluxdbv2\Client;
 
+use Icinga\Module\Perfdatagraphs\Icingadb\IcingaObjectHelper as IcinaDBCVH;
+use Icinga\Module\Perfdatagraphs\Ido\IcingaObjectHelper as IdoCVH;
+use Icinga\Module\Perfdatagraphs\ProvidedHook\Icingadb\IcingadbSupport;
+
 use Icinga\Application\Config;
 use Icinga\Application\Logger;
+use Icinga\Application\Modules\Module;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
@@ -29,6 +34,7 @@ class Influx
     protected string $org;
     protected string $bucket;
     protected string $token;
+    protected int $maxDataPoints;
 
     public function __construct(
         string $baseURI,
@@ -36,6 +42,7 @@ class Influx
         string $bucket,
         string $token,
         int $timeout = 2,
+        int $maxDataPoints = 10000,
         bool $tlsVerify = true
     ) {
         $this->client = new Client([
@@ -45,6 +52,7 @@ class Influx
 
         $this->URL = rtrim($baseURI, '/');
 
+        $this->maxDataPoints = $maxDataPoints;
         $this->org = $org;
         $this->bucket = $bucket;
         $this->token = $token;
@@ -65,10 +73,18 @@ class Influx
                |> filter(fn: (r) => r["service"] == "%s")', $this->bucket, $from, $checkCommand, $hostName, $serviceName);
 
         // We need a different aggregateWindow function for different fields
-        $q .= sprintf('getSet = (tables=<-, field, fn) =>
-    tables
-        |> filter(fn: (r) => r._field == field)
-        |> aggregateWindow(fn: fn, every: %s)', '10m');
+        $q .= 'getSet = (tables=<-, field, fn) => tables
+        |> filter(fn: (r) => r._field == field)';
+
+        var_dump($this->maxDataPoints);
+        if ($this->maxDataPoints > 0) {
+            $windowEverySeconds = $this->getAggregateWindow($hostName, $serviceName, $checkCommand, $isHostCheck, $from);
+
+            var_dump($windowEverySeconds);
+            if ($windowEverySeconds > 0) {
+                $q .= sprintf('|> aggregateWindow(fn: fn, every: %ss)', $windowEverySeconds);
+            }
+        }
 
         // unit is a string, we cannot use mean
         $q .= sprintf('
@@ -171,6 +187,42 @@ class Influx
         return $ts->getTimestamp();
     }
 
+    protected function getAggregateWindow(string $hostName, string $serviceName, string $checkCommand, bool $isHostCheck, string $from): int
+    {
+        $checkIntervalInSeconds = 60;
+
+        // We need to fetch the check interval from the database
+        if (Module::exists('icingadb') && IcingadbSupport::useIcingaDbAsBackend()) {
+            Logger::debug('Used IcingaDB as database backend');
+            $cvh = new IcinaDBCVH();
+            $object = $cvh->getObjectFromString($hostName, $serviceName, $isHostCheck);
+            $checkIntervalInSeconds = $object->check_interval;
+        } else {
+            Logger::debug('Used IDO as database backend');
+            $cvh = new IdoCVH();
+            $object = $cvh->getObjectFromString($hostName, $serviceName, $isHostCheck);
+
+            if ($isHostCheck) {
+                $checkIntervalInSeconds = $object->host_check_interval;
+            } else {
+                $checkIntervalInSeconds = $object->service_check_interval;
+            }
+        }
+
+        $now = (new DateTime())->getTimestamp();
+        $from = intval($from);
+        // Estimate of how many datapoints we have in the given time range given the check interval
+        // TODO: This only works if there is actual data
+        // Alternatively we could run a COUNT query()
+        $numOfDatapoints = round(($now - $from) / $checkIntervalInSeconds);
+        // If there are datapoints than allowed we calculate an aggregation window size
+        if ($numOfDatapoints > $this->maxDataPoints) {
+            return (int) round(($now - $from) / $this->maxDataPoints);
+        }
+
+        return 0;
+    }
+
     /**
      * fromConfig returns a new Influx Client from this module's configuration
      *
@@ -185,6 +237,7 @@ class Influx
             'api_bucket' => '',
             'api_org' => '',
             'api_token' => '',
+            'api_max_data_points' => 10000,
             'api_tls_insecure' => false,
         ];
 
@@ -201,12 +254,13 @@ class Influx
 
         $baseURI = rtrim($moduleConfig->get('influx', 'api_url', $default['api_url']), '/');
         $timeout = (int) $moduleConfig->get('influx', 'api_timeout', $default['api_timeout']);
+        $maxDataPoints = (int) $moduleConfig->get('graphite', 'api_max_data_points', $default['api_max_data_points']);
         $org = $moduleConfig->get('influx', 'api_org', $default['api_org']);
         $bucket = $moduleConfig->get('influx', 'api_bucket', $default['api_bucket']);
         $token = $moduleConfig->get('influx', 'api_token', $default['api_token']);
         // Hint: We use a "skip TLS" logic in the UI, but Guzzle uses "verify TLS"
         $tlsVerify = !(bool) $moduleConfig->get('influx', 'api_tls_insecure', $default['api_tls_insecure']);
 
-        return new static($baseURI, $org, $bucket, $token, $timeout, $tlsVerify);
+        return new static($baseURI, $org, $bucket, $token, $timeout, $maxDataPoints, $tlsVerify);
     }
 }
